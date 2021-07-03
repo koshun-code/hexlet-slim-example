ae02d84b8fac3dbc152a809ac32a3f1bd9d6cee0
Konstantin <sit.com.k@gmail.com>
2021-07-03 22:00:33 +0300

    first step

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__. '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+$app->run();
